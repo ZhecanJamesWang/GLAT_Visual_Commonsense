6122706ed6f2adee9f31369bb93a548ff1e7348b
ZhecanJamesWang <tangtangwzc@gmail.com>
2019-10-23 23:18:03 -0400

    add glat structure

--- layers.py
+++ layers.py
@@ -59,21 +59,18 @@ class ScaledDotProductAttention(nn.Module):
 
     def forward(self, q, k, v, mask=None):
 
+        # (b，n, d) (b, d, n) = (b, n, n)
         attn = torch.bmm(q, k.transpose(1, 2))
         attn = attn / self.temperature
 
-        # import pdb
-        # print("mask.shape: ", mask.shape)
-        # print("attn.shape: ", attn.shape)
-        # print("q.shape: ", q.shape)
-        # print("k.shape: ", k.shape)
-        # pdb.set_trace()
 
-        # if mask is not None:
-        #     attn = attn.masked_fill(mask, -np.inf)
+        if mask is not None:
+            attn = attn.masked_fill(mask, -np.inf)
 
         attn = self.softmax(attn)
         attn = self.dropout(attn)
+
+        # (b, n, n) (b, n, d) = (b, n, d)
         output = torch.bmm(attn, v)
 
         return output, attn
@@ -122,9 +119,7 @@ class MultiHeadAttention(nn.Module):
 
         residual = q
 
-        # print(sz_b, len_q, n_head, d_k)
-        # print(sz_b*len_q*n_head*d_k)
-
+        # (b, n, d) (d, nhead*k) => (b, n, nhead*k)
         q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
         k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
         v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
@@ -186,6 +181,7 @@ class EncoderLayer(nn.Module):
     def forward(self, enc_input, non_pad_mask=None, slf_attn_mask=None):
         # enc_output, enc_slf_attn = self.slf_attn(
         #     enc_input, enc_input, enc_input)
+
         enc_output, enc_slf_attn = self.slf_attn(
             enc_input, enc_input, enc_input, mask=slf_attn_mask)
 
@@ -205,6 +201,7 @@ class GraphAttentionLayer(nn.Module):
 
     def __init__(self, in_features, out_features, dropout, alpha, concat=True):
         super(GraphAttentionLayer, self).__init__()
+
         self.dropout = dropout
         self.in_features = in_features
         self.out_features = out_features
@@ -220,21 +217,34 @@ class GraphAttentionLayer(nn.Module):
 
         self.fc =  nn.Linear(2 * out_features, out_features)
 
+    # (b, G_n, N_n, N_n) (b, G_n, N_n, d)
+
     def forward(self, input, adj):
         # pdb.set_trace()
+
+
+        # input (b, N_n, d)
+
+        # input (b*n, N_n, d) W (d, d) => h (b*n, N_n, d)
         h = torch.matmul(input, self.W)
         # N = h.size()[0]
         B = h.size()[0]
         N = h.size()[1]
         # D = h.size()[2]
 
+        # h(b, N_n, d) => a_input (b, N_n, N_n, 2*d)
         a_input = torch.cat([h.repeat(1, 1, N).view(B, N * N, -1), h.repeat(1, N, 1)], dim=2).view(B, N, -1, 2 * self.out_features)
 
+
+        # a_input(b, N_n, N_n, 2*d) *  a (2*d, 1) = (b, N_n, N_n, 1) => e (b, N_n, N_n)
         e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(3))
 
         zero_vec = -9e15*torch.ones_like(e)
 
+        # adj (B, N_n, N_n) attention (b, N_n, N_n) e (b, N_n, N_n) zero vec(b, N_n, N_n)
         attention = torch.where(adj > 0, e, zero_vec)
+
+        # (b * n, N_n, N_n)
 
         attention = F.softmax(attention, dim=2)
         attention = F.dropout(attention, self.dropout, training=self.training)
@@ -244,6 +254,7 @@ class GraphAttentionLayer(nn.Module):
 
         # h_prime = torch.matmul(attention, h) + h
 
+        # attention (B, N_n, N_n) * h (b, N_n, d) => (b, N_n, d)
         h_prime = torch.cat((torch.matmul(attention, h), h), 2)
         h_prime = self.fc(h_prime)
 
@@ -251,6 +262,11 @@ class GraphAttentionLayer(nn.Module):
             return F.elu(h_prime)
         else:
             return h_prime
+
+
+        # (b*n, N_n, d) (b*n, d, N_n) = (b*n, N_n, N_n)
+
+        # adj => (b, N_n, N_n) => (b*n, N_n, N_n)
 
 
         # a_input = torch.cat([h.repeat(1, N).view(N * N, -1), h.repeat(N, 1)], dim=1).view(N, -1, 2 * self.out_features)
@@ -271,3 +287,111 @@ class GraphAttentionLayer(nn.Module):
     def __repr__(self):
         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
 
+class GraphAtt_Mutlihead_Basic(nn.Module):
+    """
+    Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
+    """
+
+    def __init__(self, n_head, d_model, d_k, d_v, foc_type, dropout=0.1):
+        super(GraphAtt_Mutlihead_Basic, self).__init__()
+
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.w_qs = nn.Linear(d_model, n_head * d_k)
+        self.w_ks = nn.Linear(d_model, n_head * d_k)
+        self.w_vs = nn.Linear(d_model, n_head * d_v)
+
+        self.attention = GraphAtt_ScaledDotProduct(np.power(d_k, 0.5), dropout, foc_type)
+        self.layer_norm = nn.LayerNorm(d_model)
+
+        self.fc = nn.Linear(n_head * d_v, d_model)
+        # nn.init.xavier_normal_(self.fc.weight)
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.fc1 = nn.Linear(2 * d_model, d_model)
+
+    def forward(self, q, k, v, adj, mask=None):
+
+        # (b, N_n, d)
+
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+
+        sz_b, len_q, _ = q.size()
+        sz_b, len_k, _ = k.size()
+        sz_b, len_v, _ = v.size()
+
+        residual = q
+
+        # (b, n, d) (d, nhead*k) => (b, n, nhead*k)
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        q = q.permute(2, 0, 1, 3).contiguous().view(-1, len_q, d_k) # (n*b) x lq x dk
+        k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k) # (n*b) x lk x dk
+        v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v) # (n*b) x lv x dv
+
+        mask = mask.repeat(n_head, 1, 1) # (n*b) x .. x ..
+        output, attn = self.attention(q, k, v, adj, mask=mask)
+        # output, attn = self.attention(q, k, v)
+
+        output = output.view(n_head, sz_b, len_q, d_v)
+        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1) # b x lq x (n*dv)
+
+        output = self.dropout(self.fc(output))
+
+        # output = self.layer_norm(output + residual)
+
+        output = torch.cat((output, residual), 2)
+        output = self.layer_norm(self.fc1(output))
+
+        return output, attn
+
+    # def __repr__(self):
+    #     return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+
+
+class GraphAtt_ScaledDotProduct(nn.Module):
+    """
+    Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
+    """
+
+    def __init__(self, temperature, dropout, foc_type, concat=True):
+        super(GraphAtt_ScaledDotProduct, self).__init__()
+
+        self.temperature = temperature
+        self.dropout = nn.Dropout(dropout)
+        self.softmax = nn.Softmax(dim=2)
+        self.foc_type = foc_type
+
+    def forward(self, q, k, v, adj, mask=None):
+        a, N_n, _ = v.size()
+        b, _, _ = adj.size()
+        nhead = int(a/b)
+
+        # (b*n, N_n, d) (b*n, d, N_n) = (b*n, N_n, N_n)
+        attn = torch.bmm(q, k.transpose(1, 2))
+        attn = attn / self.temperature
+
+        if mask is not None:
+            attn = attn.masked_fill(mask, -np.inf)
+
+        if self.foc_type == "local":
+            # adj => (b, N_n, N_n) => (b*n, N_n, N_n)
+            adj = adj.unsqueeze(0)
+            adj = adj.repeat(nhead, 1, 1, 1)
+            adj = adj.view(-1, N_n, N_n)
+
+            zero_vec = -9e15*torch.ones_like(attn)
+            attn = torch.where(adj > 0, attn, zero_vec)
+
+        attn = self.softmax(attn)
+        attn = self.dropout(attn)
+
+        # attention (b*n, N_n, N_n) v (b*n, N_n, d) => (b*n, N_n, d)
+        output = torch.bmm(attn, v)
+
+        return output, attn
